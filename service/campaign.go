4eb98989e4a87511b44e0853d8a1cf87cb59c014
Hafidh Pradipta A <hamstergeek38@gmail.com>
2023-08-22 16:47:32 +0700

    feat: implement service campaign

---
+++ service/campaign.go
@@ -0,0 +1,38 @@
+package service
+
+import (
+	"github.com/alrasyidin/bwa-backer-startup/db/models"
+	"github.com/alrasyidin/bwa-backer-startup/repository"
+)
+
+type ICampaignService interface {
+	FindCampaigns(UserID int) (models.Campaign, error)
+}
+
+type CampaignService struct {
+	repo repository.ICampaignRepo
+}
+
+// Constructor for CampaignService
+func NewCampaignService(repo repository.ICampaignRepo) *CampaignService {
+	return &CampaignService{
+		repo,
+	}
+}
+
+func (service *CampaignService) FindCampaigns(UserID int) ([]models.Campaign, error) {
+	if UserID != 0 {
+		campaigns, err := service.repo.FindByUserID(UserID)
+
+		if err != nil {
+			return campaigns, err
+		}
+
+		return campaigns, nil
+	}
+	campaigns, err := service.repo.FindAll()
+	if err != nil {
+		return campaigns, err
+	}
+	return campaigns, nil
+}
